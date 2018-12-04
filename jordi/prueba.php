b12df555a014b78d71b0f9b312d8b5ca4dea3180
root <jmartinezmateos@iesesteveterradas.cat>
2018-12-04 17:54:07 +0100

    formulario se crea a traves de js

--- jordi/prueba.php
+++ jordi/prueba.php
@@ -8,37 +8,14 @@
 </head>
 <body>
 
-	<input type="button" name="crearProyecto" value="Crear nuevo proyecto">
+	<input type="button" name="crearProyecto" value="Crear nuevo proyecto" id="botonCrearProyecto" onclick="crearFormulario()">
 
-	<div class="formulario">
-		<h1 class="titulo">Formulario</h1>
-		<div class="formulario_izquierda">
-			<br>
-			<label for="nombre">Nombre</label>
-			<input type="text" name="nombre">
-			<br><br>
-			<label for="descripcion">Descripcion</label>
-			<textarea name="descripcion" cols="30"></textarea>
+	<div id="formulario" hidden>
+
+		<div id="formulario_izquierda">
 		</div>
-		<div class="formulario_derecha">
-			
-			<select>
-				<option>Scrum</option>
-				<option>prueba</option>
-			</select>
-			<br>
-			<select>
-				<option>Project</option>
-				<option>prueba</option>
-			</select>
-			<br><br>
-			<input type="radio" name="developer1" value="developer1">developer1
-			<br>
-			<input type="radio" name="developer2" value="developer2">developer2
-			<br>
+		<div id="formulario_derecha">
 		</div>
-
-		<input type="button" name="crear" value="Crear" class="boton_crear_dentro">
-
+	</div>
 </body>
 </html>
