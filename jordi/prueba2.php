f5088f56e372ce8bac6a1386eb462680a273be75
unknown <jmartinezmateos@iesesteveterradas.cat>
2018-12-10 14:43:44 +0100

    algo

--- jordi/prueba2.php
+++ jordi/prueba2.php
@@ -5,6 +5,12 @@
 </head>
 <body>
 	<?php 
+	//he tenido que hacer en el phpmyadmin
+	/*
+	INSERT INTO `users`(`id_user`, `user`, `password`, `rol`, `name`, `last_name`, `mail`, `phone_number`) VALUES (11,'jmartinez','1234','Developer','Jordi','martinez','ssa@a',123)
+	INSERT INTO `proj_users`(`id_proj_user`, `user`, `cod_project`, `name_proj`) VALUES (5,'jmartinez','30','qui2')
+	INSERT INTO `project`(`id_project`, `cod_project`, `name_project`, `description`, `product_owner`, `scrum_master`, `date_start`, `date_finish`, `comments`, `budget`) VALUES (3,30,'nombre_proy','descripcion','emieza','lzabala',now(),now(),'xcomentario',1000)
+	*/
 
 		$nombre_proyecto = $_POST["nombre"];
 		$campo_scrum_master = $_POST["campo_scrum_master"];
@@ -23,5 +29,22 @@
                     echo "<br>$value";
                 }
 	 ?>
+
+	<?php
+		$sentencia = $mbd->prepare("INSERT INTO REGISTRY (name, value) VALUES (:name, :value)");
+		$sentencia->bindParam(':name', $nombre);
+		$sentencia->bindParam(':value', $valor);
+
+		// insertar una fila
+		$nombre = 'uno';
+		$valor = 1;
+		$sentencia->execute();
+
+		// insertar otra fila con diferentes valores
+		$nombre = 'dos';
+		$valor = 2;
+		$sentencia->execute();
+	?>
+
 </body>
 </html>
